b7a7b08f2ab617e378cdb6257debb3ffad8e57e7
Dennis <dennis@cavok.home>
2018-07-31 08:09:10 +0100

    Moved mock BLE client to separate file

---
+++ src/github.com/delta-bravo/blue-weather/bleservices/ble_mock_test.go
@@ -0,0 +1,98 @@
+package bleservices_test
+
+import "github.com/go-ble/ble"
+
+type TestBleClient struct {
+	discoverProfileParams DiscoverProfileParams
+	subscribeParams       SubscribeParams
+}
+
+func (TestBleClient) Addr() ble.Addr {
+	panic("implement me")
+}
+
+func (TestBleClient) Name() string {
+	panic("implement me")
+}
+
+func (TestBleClient) Profile() *ble.Profile {
+	panic("implement me")
+}
+
+func (client *TestBleClient) DiscoverProfile(force bool) (*ble.Profile, error) {
+	client.discoverProfileParams = DiscoverProfileParams{
+		called:    true,
+		withForce: force,
+	}
+	return nil, nil
+}
+
+func (TestBleClient) DiscoverServices(filter []ble.UUID) ([]*ble.Service, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) DiscoverIncludedServices(filter []ble.UUID, s *ble.Service) ([]*ble.Service, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) DiscoverCharacteristics(filter []ble.UUID, s *ble.Service) ([]*ble.Characteristic, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) DiscoverDescriptors(filter []ble.UUID, c *ble.Characteristic) ([]*ble.Descriptor, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) ReadCharacteristic(c *ble.Characteristic) ([]byte, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) ReadLongCharacteristic(c *ble.Characteristic) ([]byte, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) WriteCharacteristic(c *ble.Characteristic, value []byte, noRsp bool) error {
+	panic("implement me")
+}
+
+func (TestBleClient) ReadDescriptor(d *ble.Descriptor) ([]byte, error) {
+	panic("implement me")
+}
+
+func (TestBleClient) WriteDescriptor(d *ble.Descriptor, v []byte) error {
+	panic("implement me")
+}
+
+func (TestBleClient) ReadRSSI() int {
+	panic("implement me")
+}
+
+func (TestBleClient) ExchangeMTU(rxMTU int) (txMTU int, err error) {
+	panic("implement me")
+}
+
+func (client *TestBleClient) Subscribe(c *ble.Characteristic, ind bool, h ble.NotificationHandler) error {
+	subscribeParams := SubscribeParams{
+		characteristic: *c,
+		ind:            ind,
+		handler:        h,
+	}
+	client.subscribeParams = subscribeParams
+	return nil
+}
+
+func (TestBleClient) Unsubscribe(c *ble.Characteristic, ind bool) error {
+	panic("implement me")
+}
+
+func (TestBleClient) ClearSubscriptions() error {
+	panic("implement me")
+}
+
+func (TestBleClient) CancelConnection() error {
+	panic("implement me")
+}
+
+func (TestBleClient) Disconnected() <-chan struct{} {
+	panic("implement me")
+}
